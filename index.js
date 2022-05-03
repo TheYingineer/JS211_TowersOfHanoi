365f9f3f722ae8ecdd69cf6f35a5451a5f785801
TheYingineer <ying.yan.huang87@gmail.com>
2022-05-02 22:34:28 -0500

    should be a functional game now

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 // * Why are you get a warning in your console? Fix it.
 // * Delete these comment lines!
 
-const stone = null
+let stone = null
 
 // this function is called when a row is clicked. 
 // Open your inspector tool to see what is being captured and can be used.
@@ -18,14 +18,32 @@ const selectRow = (row) => {
   console.log("Here is the stone's id: ", row.id)
   console.log("Here is the stone's data-size: ", currentRow)
 
-  pickUpStone(row.id)
-} 
+  //if current is no stone, pick up stone, else drop stone
+  if(!stone){ 
+    return pickUpStone(row.id)
+    }
 
-// this function can be called to get the last stone in the stack
-// but there might be something wrong with it...
+  //if the row's last stone color is not the same as the current color, drop the stone
+  if(!row.lastElementChild&&stone){
+    dropStone(row.id);
+    checkForWin();
+    return;
+  }
+
+  //Another method to check only apply in the HTML "is it Legal?"
+  //comparing the id number by index.html
+  //if the id number is bigger than the other, drop the stone
+  if(row.lastElementChild.getAttribute("id")>stone.getAttribute("id")){
+    console.log(row.lastElementChild.getAttribute("id"), "is greater than", stone.getAttribute("id"))
+    dropStone(row.id);
+    checkForWin();
+    return;
+  }
+}
+
 const pickUpStone = (rowID) => {
   const selectedRow = document.getElementById(rowID);
-  stone = selectedRow.removeChild(selectedRow.lastChild);
+  stone = selectedRow.removeChild(selectedRow.lastElementChild);
   console.log(stone)
 }
 
@@ -33,10 +51,17 @@ const pickUpStone = (rowID) => {
 // Once you figure that out you'll need to figure out if its a legal move...
 // Something like: if(!stone){pickupStone} else{dropStone}
 
-const dropStone = (rowID, stone) => {
+const dropStone = (rowID) => {
   document.getElementById(rowID).appendChild(stone)
-  stone = null
+  stone = null // <== this line basically saying reset the stone value to null/0
 }
 
+//childElementCount is counting how many divs under the id "top-row" or "middle-row"
+//if either "top-row" or "middle-row" has all 4 elementcount, then they win. 
+const checkForWin =() => {
+  if(document.getElementById("top-row").childElementCount === 4 || document.getElementById("middle-row").childElementCount === 4) {
+  console.log("You Win!")
+  }
+}
 // * Remember you can use your logic from 'main.js' to maintain the rules of the game. But how? Follow the flow of data just like falling dominoes.
 
--- main.js
+++ main.js
@@ -30,27 +30,65 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
+const movePiece = (startStack, endStack) => {
 
+   //to target the property startStack insdie of stake
+   console.log(stacks[startStack], stacks[endStack])
+
+   // remove last item from the start stack
+  let lastItem = stacks[startStack].pop() 
+  // console.log(stacks)
+  console.log(lastItem)
+
+   // add remove item to endStack
+  stacks[endStack].push(lastItem)
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
+const isLegal = (startStack, endStack) => {
+
+  //for as long as if Startstack is not empty
+  if(stacks[startStack].length>0){
+      //if endstack is empty, return it true that can you move
+      if(stacks[endStack].length==0){
+        return true;
+      }
+
+     // .slice(-1) is the last element in the array
+     // below is to compare the last element in startstack with the last element in the endstack, 
+     // if the first stack's very last element is smaller than the end stack's very last element, return true that you can move
+      if(stacks[startStack].slice(-1)<stacks[endStack].slice(-1)){
+        return true;
+      }
+
+      else{
+        return false;
+      }
+  }
 
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
-  // Your code here
-
+//check and see if b[1 2 3 4] or c[1 2 3 4] both have all 4 element as in an array
+// if the length of the array is equal to 4, then it's full. I don't need to know if it's legal anymore because
+// islegal function took care of that already. 
+  if((stacks["b"].length ==4)||(stacks["c"].length == 4)){
+    console.log("Hey you won! :)")
+    return true;
+    }
+    else{
+      return false;
+    }
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
 
+if(isLegal(startStack, endStack)){
+    movePiece(startStack, endStack)
+  } 
+  checkForWin()
 }
 
 const getPrompt = () => {
